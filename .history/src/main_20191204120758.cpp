242aae754e2bdf001e7926065fb5045c1b88d368
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-12-04 12:11:41 +0100

    Initial commit

---
+++ .history/src/main_20191204120758.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "DHT.h"
+
+#define DHTPIN 2  
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
